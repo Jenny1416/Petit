36367ad2e46071c5789ae9cf8484f4df48170a3f
Jenny1416 <jeniferp-lopeza@unilibre.edu.co>
2026-04-19 17:51:45 -0500

    Version 1.1 del modulo auth

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,11 @@
+class UserModel {
+  final String email;
+  final String password;
+  final String phone;
+
+  UserModel({
+    required this.email,
+    required this.password,
+    required this.phone,
+  });
+}
